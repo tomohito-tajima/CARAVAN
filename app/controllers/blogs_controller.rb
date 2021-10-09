1741068bbbf6a9f69dcd94be3790a7edcf4af2a1
tomohito-tajima <tomo322017@gmail.com>
2021-10-09 07:59:45 +0000

    Finish 3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -29,7 +29,13 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
-  end  
+  end
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
     
   private
   def blog_params
